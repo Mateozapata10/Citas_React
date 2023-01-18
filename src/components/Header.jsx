4ac8262587cccd3d35bf3c3d30133709c077eee7
Mateozapata10 <mateozapatalrc@gmail.com>
2023-01-18 12:53:54 -0500

    Cambio de header agregando un de

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,8 @@ function Header()
     
 
     return (
-        <h1 className="font-black text-5xl text-center md:w-2/3 w-1/2 mx-auto">Seguimiento pacientes {''}
-            <span className="text-indigo-600 uppercase">Veterinaria</span>
+        <h1 className="font-black text-5xl text-center md:w-2/3 w-1/2 mx-auto">Seguimiento de pacientes {''}
+            <span className="text-indigo-600 uppercase">Veterinaria </span>
         </h1>
     )
 }
